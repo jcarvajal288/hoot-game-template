2818d825ef39cc72deefa72e498443e9a314ef56
Max Carvajal <jcarvajal288@gmail.com>
2024-08-14 20:56:53 -0500

    Add in stdlib from puyolab

--- modules/stdlib/list.scm
+++ modules/stdlib/list.scm
@@ -1,11 +1,50 @@
 (define-module (stdlib list)
   #:pure
   #:use-module (scheme base)
-  #:export (filter))
+  #:use-module (scheme write)
+  #:export (filter
+            range
+            empty?
+            foldl
+            contains?
+            get-longest-sublist
+            none?))
 
 (define (filter fn lst)
   (cond ((null? lst) '())
         ((fn (car lst)) 
-         (cons (car lst)
-               (filter fn (cdr lst))))
+           (cons (car lst)
+                 (filter fn (cdr lst))))
         (else (filter fn (cdr lst)))))
+
+(define (range min max)
+  (if (>= min max)
+      '()
+      (cons min (range (+ min 1) max))))
+
+(define (empty? lst)
+  (= (length lst) 0))
+
+(define (foldl fn initial lst)
+  (if (empty? lst)
+      initial
+      (foldl fn (fn initial (car lst)) (cdr lst))))
+
+(define (contains? lst obj)
+  (not (not (memq obj lst))))
+
+(define (get-longest-sublist lists)
+  (define (reducer current-longest lists)
+    (cond ((empty? lists)
+            current-longest)
+          ((> (length (car lists)) (length current-longest))
+            (reducer (car lists) (cdr lists)))
+          (else
+            (reducer current-longest (cdr lists)))))
+  (reducer '() lists))
+
+(define (none? lst)
+  (not (foldl (lambda (x y)
+                (or x y))
+              #f
+              lst)))
